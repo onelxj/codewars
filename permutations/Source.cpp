eec191e6760f95a4819dadb90fd17fa8f22e4048
Zakhar <56281186+onelxj@users.noreply.github.com>
2019-12-29 16:47:15 +0300

    Update Source.cpp

--- permutations/Source.cpp
+++ permutations/Source.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 long double fact(int N)
 {
-    if(N < 0) // если пользователь ввел отрицательное число
-        return 0; // возвращаем ноль
-    if (N == 0) // если пользователь ввел ноль,
-        return 1; // возвращаем факториал от нуля - не удивляетесь, но это 1 =)
-    else // Во всех остальных случаях
-        return N * fact(N - 1); // делаем рекурсию.
+    if(N < 0)
+        return 0;
+    if (N == 0)
+        return 1;
+    else
+        return N * fact(N - 1);
 }
 
 bool in_data(string s, vector<string> d) {
